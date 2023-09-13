e801a7c47faff7e649aa79d1f379375cda74d325
LauroRambo <lauroorambo@gmail.com>
2023-09-13 09:37:29 -0300

    Incluindo seguimores

---
+++ api/sql/dados.sql
@@ -0,0 +1,12 @@
+insert into usuarios (nome, nick, email, senha)
+VALUES
+("User 1", "user_1", "user_1@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S"),
+("User 2", "user_2", "user_2@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S"),
+("User 3", "user_3", "user_3@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S");
+
+
+insert into seguidores (usuairo_id, seguidor_id)
+VALUES
+(1, 2),
+(3, 1),
+(1, 3);
