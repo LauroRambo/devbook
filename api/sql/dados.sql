0c072fa4003b17d79562ff117c839fac59c608e9
LauroRambo <lauroorambo@gmail.com>
2023-09-14 12:25:32 -0300

    Incrementando crud publicacao

--- api/sql/dados.sql
+++ api/sql/dados.sql
@@ -1,8 +1,8 @@
 insert into usuarios (nome, nick, email, senha)
 VALUES
-("User 1", "user_1", "user_1@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S"),
-("User 2", "user_2", "user_2@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S"),
-("User 3", "user_3", "user_3@gmail.com", "$2a$10$4OAQIOii71MdJy6TMqsyY.NcyufPVpABzicepVR45CBhrDWQiwW3S");
+("User 1", "user_1", "user_1@gmail.com", "$2a$10$UNGtFs6X.EhtRxo4jzEIFukw/31LvnO/1lB3rH2mwA.hgbwtEsGqm"),
+("User 2", "user_2", "user_2@gmail.com", "$2a$10$UNGtFs6X.EhtRxo4jzEIFukw/31LvnO/1lB3rH2mwA.hgbwtEsGqm"),
+("User 3", "user_3", "user_3@gmail.com", "$2a$10$UNGtFs6X.EhtRxo4jzEIFukw/31LvnO/1lB3rH2mwA.hgbwtEsGqm");
 
 
 insert into seguidores (usuario_id, seguidor_id)
@@ -10,3 +10,10 @@ VALUES
 (1, 2),
 (3, 1),
 (1, 3);
+
+
+insert into publicacoes(titulo, conteudo, autor_id)
+values
+("Publicacão do user ", "Essa é a Publicacão do user 1", 1),
+("Publicacão do user 2", "Essa é a Publicacão do user 2", 2),
+("Publicacão do user 3", "Essa é a Publicacão do user 3", 3);
--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -7,6 +7,7 @@ import (
 	"api/src/repositorios"
 	"api/src/respostas"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"strconv"
@@ -59,7 +60,27 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.Buscar(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
 }
 
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
@@ -88,9 +109,126 @@ func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
 }
 
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicacaoSalvaNoBanco.AutorID != usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel atualizar uma publicacão que não seja sua"))
+		return
+	}
+
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = publicacao.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = repositorio.Atualizar(publicacaoID, publicacao); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
 
 }
 
 func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacaoSalvaNoBanco, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicacaoSalvaNoBanco.AutorID != usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possivel deletar uma publicacão que não seja sua"))
+		return
+	}
+
+	if erro = repositorio.Deletar(publicacaoID); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+}
+
+func BuscarPublicacoesPorUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.BuscarPorUsuario(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
 }
